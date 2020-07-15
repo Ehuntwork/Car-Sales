853df26c3289c506c760236827a11f5b12ad8e73
emilme <emilme@sir.mle>
2020-07-15 13:55:31 -0700

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux"; 
+import {removeFeature, addFeature} from './actionTitles/actionTitles'
+
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
@@ -7,14 +9,15 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = (props) => {
+  console.log(addFeature)
   return (
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <AddedFeatures car={props.car} removeFeature={props.removeFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} addFeature={props.addFeature}/>
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
@@ -25,8 +28,9 @@ const mapStateToProps = state => {
   console.log(state)
   return {
     additionalFeatures: state.additionalFeatures,
-    car: state.car
+    car: state.car,
+    additionalPrice: state.additionalPrice
   };
 };
 
-export default connect(mapStateToProps,{})(App);
+export default connect(mapStateToProps,{addFeature, removeFeature})(App);
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,6 @@
-      //move these between each other
+  import {ADD_FEATURE, REMOVE_FEATURE } from '../actionTitles/actionTitles'
+
+
   const initialState = {
     additionalPrice: 0,
     car: {
@@ -19,6 +21,32 @@
   export const reducer = (state = initialState, action) => {
     console.log(state, action);
     switch (action.type) {
+        case ADD_FEATURE:
+            console.log('a',action.payload)
+            console.log('a',state)
+
+            return {
+                ...state, 
+                car:{ 
+                    ...state.car,
+                    features:[...state.car.features, action.payload]
+                },
+                additionalFeatures: state.additionalFeatures.filter(item=>item.id !== action.payload.id),
+                additionalPrice: state.additionalPrice += action.payload.price
+            }
+        case REMOVE_FEATURE:
+            console.log('r',action.payload)
+            console.log('r',state)
+            return{
+                ...state, 
+                car:{ 
+                    ...state.car,
+                    features:state.car.features.filter(item=>item.id !== action.payload.id)
+                },
+                additionalFeatures:[...state.additionalFeatures, action.payload],
+                additionalPrice: state.additionalPrice -= action.payload.price
+
+            }
       default:
         return state;
     }
